f94f35543809872a3bee7ea9f6fc5937ef48976d
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-05-13 19:02:46 -0700

    add ActivityPanel functionality

--- src/components/ActivityPanel.jsx
+++ src/components/ActivityPanel.jsx
@@ -1,7 +1,20 @@
-import { useState } from 'react';
-
-export default function ActivityPanel({ todos, dispatch }) {
+export default function ActivityPanel({ todos, dispatch, show, setShow }) {
   const countOfCompletedTodos = todos.filter((todo) => !todo.completed).length;
+
+  function showAllTasks() {
+    Object.keys(show).forEach((key) => (show[key] = false));
+    setShow((prevState) => ({ all: true }));
+  }
+
+  function showActiveTasks() {
+    Object.keys(show).forEach((key) => (show[key] = false));
+    setShow({ completed: true });
+  }
+
+  function showCompletedTasks() {
+    Object.keys(show).forEach((key) => (show[key] = false));
+    setShow({ active: true });
+  }
 
   return (
     <div className="flex w-full items-center justify-between rounded-b-md bg-light-bg-primary py-3 px-4 dark:bg-dark-bg-primary">
@@ -11,14 +24,26 @@ export default function ActivityPanel({ todos, dispatch }) {
           : `${countOfCompletedTodos} items left`}
       </span>
 
-      <div className="hidden space-x-4 text-xs  text-light-text-primary hover:text-light-text-tertiary dark:text-dark-text-quaternary hover:dark:text-dark-text-secondary md:inline-block">
-        <button type="button" className="font-bold">
+      <div className="hidden space-x-4 md:inline-block">
+        <button
+          type="button"
+          className="text-xs font-bold text-light-text-primary hover:text-light-text-tertiary dark:text-dark-text-quaternary hover:dark:text-dark-text-secondary"
+          onClick={showAllTasks}
+        >
           All
         </button>
-        <button type="button" className="font-bold">
+        <button
+          type="button"
+          className="text-xs font-bold text-light-text-primary hover:text-light-text-tertiary dark:text-dark-text-quaternary hover:dark:text-dark-text-secondary"
+          onClick={showActiveTasks}
+        >
           Active
         </button>
-        <button type="button" className="font-bold">
+        <button
+          type="button"
+          className="text-xs font-bold text-light-text-primary hover:text-light-text-tertiary dark:text-dark-text-quaternary hover:dark:text-dark-text-secondary"
+          onClick={showCompletedTasks}
+        >
           Completed
         </button>
       </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,43 @@
+import { useState } from 'react';
 // components
 import TodoItem from './TodoItem';
 import ActivityPanel from './ActivityPanel';
 
 export default function TodoList({ todos, dispatch }) {
+  const [show, setShow] = useState([
+    { all: true, active: false, completed: false },
+  ]);
+
   const allToDos = todos.map((todo, index) => (
     <TodoItem key={todo.id} {...todo} index={index} dispatch={dispatch} />
   ));
 
+  const activeTodos = todos
+    .filter((todo) => todo.completed)
+    .map((todo, index) => (
+      <TodoItem key={todo.id} {...todo} index={index} dispatch={dispatch} />
+    ));
+
+  const completedTodos = todos
+    .filter((todo) => !todo.completed)
+    .map((todo, index) => (
+      <TodoItem key={todo.id} {...todo} index={index} dispatch={dispatch} />
+    ));
+
   return (
     <div className="mt-4">
-      <ul className="w-full">{allToDos}</ul>
+      <ul className="w-full">
+        {show.active ? activeTodos : show.completed ? completedTodos : allToDos}
+      </ul>
 
-      {todos.length > 0 && <ActivityPanel todos={todos} dispatch={dispatch} />}
+      {todos.length > 0 && (
+        <ActivityPanel
+          todos={todos}
+          dispatch={dispatch}
+          show={show}
+          setShow={setShow}
+        />
+      )}
     </div>
   );
 }
